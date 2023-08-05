f7f09494ff23088127f37d35bfd7024702a4022f
fauzandp <fauzan27dp@gmail.com>
2023-08-05 14:41:40 +0700

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Fauzan Dwi Prasetyo on 05/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
